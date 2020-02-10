2dfa21989fb3d440e6102cb8b150e9759c014fc4
Vivica Ma <vivicacongma@gmail.com>
2020-02-10 02:06:07 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,7 +2,7 @@
 first_number = 1 
 second_number = 4
 sum = first_number + second_number
-differnce = first_number - second_number
+difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
 
